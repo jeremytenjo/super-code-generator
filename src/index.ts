3f4580b60e6f3fe0b3abd3f67478f64485fcbe93
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-05-25 17:51:15 -0400

    added ts

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import activation from './handleActivation'
+import pkgJson from '../package.json'
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  console.log(`${pkgJson.name} activated!`)
+  activation(context)
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {
+  console.log(`${pkgJson.name} deactivated!`)
+}
+
+export { activate, deactivate }
