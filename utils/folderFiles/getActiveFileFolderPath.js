0609af844ccf2d4a7faccca109c0078b4e1f0924
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-05-25 14:46:02 -0400

    s

--- utils/folderFiles/getActiveFileFolderPath.js
+++ utils/folderFiles/getActiveFileFolderPath.js
@@ -2,7 +2,7 @@ const getActiveFilePath = require('./getActiveFilePath')
 const splitPath = require('../splitPath')
 
 /**
- * Get the folder of the currently open and active vscode file
+ * Get the folder path of the currently open and active vscode file
  */
 module.exports = function getActiveFileFolderPath() {
   const activeFilePath = getActiveFilePath()
