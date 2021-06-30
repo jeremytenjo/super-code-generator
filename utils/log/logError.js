bf22e73915dc9f76cbe6ca28034ccca55f62bdb5
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-06-30 13:58:30 -0400

    added console log

--- utils/log/logError.js
+++ utils/log/logError.js
@@ -2,6 +2,10 @@ const vscode = require('vscode')
 
 module.exports = function logError(errorMessage, options = { silent: false }) {
   if (options.silent) {
+    console.error(errorMessage)
     throw Error(errorMessage)
-  } else vscode.window.showErrorMessage(errorMessage.toString())
+  } else {
+    console.error(errorMessage.toString())
+    vscode.window.showErrorMessage(errorMessage.toString())
+  }
 }
