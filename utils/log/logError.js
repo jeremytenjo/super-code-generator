1de436faa23386b4c9ff5342436e3401a3fd1d6a
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-06-30 14:19:04 -0400

    removed console llgs

--- utils/log/logError.js
+++ utils/log/logError.js
@@ -2,10 +2,8 @@ const vscode = require('vscode')
 
 module.exports = function logError(errorMessage, options = { silent: false }) {
   if (options.silent) {
-    console.error(errorMessage)
     throw Error(errorMessage)
   } else {
-    console.error(errorMessage.toString())
     vscode.window.showErrorMessage(errorMessage.toString())
   }
 }
