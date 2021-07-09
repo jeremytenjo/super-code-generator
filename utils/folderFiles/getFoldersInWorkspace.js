3c074298adf97525dfbd5d732defc657cdd02165
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-07-08 20:31:48 -0400

    ignore node modules

--- utils/folderFiles/getFoldersInWorkspace.js
+++ utils/folderFiles/getFoldersInWorkspace.js
@@ -3,7 +3,9 @@ const getWorkspacePath = require('../workspace/getWorkspacePath')
 
 module.exports = async function getFoldersInWorkspace() {
   const workspacePath = await getWorkspacePath()
-  const foldersInWorkspace = await glob.sync(workspacePath + '/**/')
+  const foldersInWorkspace = await glob.sync(workspacePath + '/**/', {
+    ignore: ['**/node_modules/**', './node_modules/**']
+  })
 
   return foldersInWorkspace
 }
