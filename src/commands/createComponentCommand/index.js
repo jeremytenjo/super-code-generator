1a3902a2fbff90bcdff18d36dde93af610a4e557
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-09-25 17:54:00 -0400

    c

--- src/commands/createComponentCommand/index.js
+++ src/commands/createComponentCommand/index.js
@@ -1,13 +1,13 @@
-const createComponent = require('../../common/createComponent')
-const logError = require('../../../utils/log/logError')
-const getActiveFileFolderPath = require('../../../utils/folderFiles/getActiveFileFolderPath')
+const createComponent = require("../../common/createComponent");
+const logError = require("../../../utils/log/logError");
+const getActiveFileFolderPath = require("../../../utils/folderFiles/getActiveFileFolderPath");
 
 module.exports = async function createComponentCommand(
   { path: componentOutputPath } = { path: getActiveFileFolderPath() }
 ) {
   try {
-    await createComponent({ outputPath: componentOutputPath })
+    await createComponent({ outputPath: componentOutputPath });
   } catch (error) {
-    logError(error)
+    logError(error);
   }
-}
+};
