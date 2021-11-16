02bd77b5c0f7388d9e7f54b67972b1b0e43e7cc4
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-11-16 10:03:09 -0500

    added lowerCase to changeCase helper

--- src/common/createComponent/handlers/helpers/index.js
+++ src/common/createComponent/handlers/helpers/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  changeCase: require('change-case'),
+  changeCase: { ...require('change-case'), lowerCase: require('lower-case').lowerCase },
   wrapInTemplateLiteral: require('./wrapInTemplateLiteral'),
   addEmptyTemplateLiteral: require('./addEmptyTemplateLiteral'),
 }
