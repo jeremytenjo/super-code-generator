3b3bfd2083a51548afdbe37789c38b680580e40d
Jeremy Tenjo <tenjojeremy@gmail.com>
2024-01-31 10:53:31 -0500

    Refactor prettifyFile.js to use prettier.format() instead of eslint.lintText()

--- utils/folderFiles/prettifyFile.js
+++ utils/folderFiles/prettifyFile.js
@@ -1,14 +1,11 @@
 const prettier = require('prettier')
-const eslint = require('eslint')
 
 module.exports = async function prettifyFile({ prettierConfig = {}, content = '' }) {
   try {
-    let prettifiedContent = prettier.format(content, {
+    const prettifiedContent = prettier.format(content, {
       ...prettierConfig,
       parser: 'babel',
     })
-
-    prettifiedContent = await eslint.lintText(prettifiedContent)
 
     return prettifiedContent
   } catch (error) {
