f8be26300688720a221c9ee6d97f6f9613a368b7
Jeremy Tenjo <tenjojeremy@gmail.com>
2025-01-30 18:54:07 -0500

    Format configuration files for improved readability and consistency

--- example/app/.prettierrc.js
+++ example/app/.prettierrc.js
@@ -1,10 +1,9 @@
 module.exports = {
-  "printWidth": 90,
-  "trailingComma": "all",
-  "tabWidth": 2,
-  "semi": false,
-  "singleQuote": true,
-  "jsxSingleQuote": true,
-  "arrowParens": "always"
+  printWidth: 90,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: 'always',
 }
-  
