fe459a4a5fb222b3ffdbc6afcbbf3fdcb4421f5d
Jeremy Tenjo <tenjojeremy@gmail.com>
2025-01-30 19:04:51 -0500

    Refactor getWorkspacePath function for improved readability and consistency

--- utils/workspace/getWorkspacePath.ts
+++ utils/workspace/getWorkspacePath.ts
@@ -1,26 +1,26 @@
 import vscode from 'vscode'
 import path from 'path'
-import { platform } from 'process';
-import { StartsWithSlashRegex } from '../splitPath';
-import removeFirstCharacter from '../folderFiles/removeFirstCharacter';
+import { platform } from 'process'
+import { StartsWithSlashRegex } from '../splitPath'
+import removeFirstCharacter from '../folderFiles/removeFirstCharacter'
 
 export type FilePath = {
-  path: string;
-  getFileUri(): string;
-  getExtension(): string;
-  hasExtension(ext: string): boolean;
-  addFile(dir: string): void;
+  path: string
+  getFileUri(): string
+  getExtension(): string
+  hasExtension(ext: string): boolean
+  addFile(dir: string): void
 }
 
 export default function getWorkspacePath(add = ''): FilePath {
   // Get the workspace folder uri
   const folderUri = vscode.workspace.workspaceFolders![0].uri
-  
+
   // add the path to the workspace folder
-  let filePath = path.join(folderUri.path, add);
+  let filePath = path.join(folderUri.path, add)
 
   // If the platform is windows and the path starts with a slash, remove the slash
-  if(platform == "win32" && StartsWithSlashRegex.test(filePath)) {
+  if (platform == 'win32' && StartsWithSlashRegex.test(filePath)) {
     filePath = removeFirstCharacter(filePath)
   }
 
