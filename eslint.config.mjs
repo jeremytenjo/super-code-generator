e087e167d0f28c213c3b169cf766e0b22fb6365a
Jeremy Tenjo <tenjojeremy@gmail.com>
2025-01-30 19:02:47 -0500

    Update ESLint configuration and .prettierignore for improved project structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,19 @@
 /**
  * ESLint configuration for the project.
- * 
+ *
  * See https://eslint.style and https://typescript-eslint.io for additional linting options.
  */
 // @ts-check
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   {
-    ignores: [
-      "out",
-      "dist",
-      "scripts"
-    ]
+    ignores: ['out', 'dist', 'scripts'],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
-);
-
-
+)
 
 // {
 //   "env": {
