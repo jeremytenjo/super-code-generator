0937c14c6f8cb79e8b03c28f2d35e3cba27d7675
Jeremy Tenjo <tenjojeremy@gmail.com>
2025-02-04 10:19:49 -0500

    Update test configuration to use 'dist' directory and improve formatting

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,12 +1,12 @@
-import {defineConfig} from "@vscode/test-cli";
+import { defineConfig } from '@vscode/test-cli'
 
 export default defineConfig({
-    label: "Unit Tests",
-    files: 'out/src/test/**/*.test.js',
-    workspaceFolder: "./example/app",
-    mocha: {
-        ui: 'tdd',
-        timeout: 10000
-    },
-    launchArgs: ["--disable-extensions"]
+  label: 'Unit Tests',
+  files: 'dist/src/test/**/*.test.js',
+  workspaceFolder: './example/app',
+  mocha: {
+    ui: 'tdd',
+    timeout: 10000,
+  },
+  launchArgs: ['--disable-extensions'],
 })
