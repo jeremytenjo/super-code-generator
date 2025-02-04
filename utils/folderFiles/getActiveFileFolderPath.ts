bec10590b5870e29f34784377a60d1f439175ae9
Jeremy Tenjo <tenjojeremy@gmail.com>
2025-02-04 10:52:18 -0500

    Refactor getActiveFilePath function and add test function for folder path retrieval

--- utils/folderFiles/getActiveFileFolderPath.ts
+++ utils/folderFiles/getActiveFileFolderPath.ts
@@ -12,5 +12,16 @@ export default function getActiveFileFolderPath(): string {
   // remove the last element
   activeFileFolder.pop()
 
+  test()
+
   return activeFileFolder.join(sep)
+}
+
+const test = () => {
+  const activeFilePath = getActiveFilePath()
+  let activeFileFolder = splitPath(activeFilePath)
+
+  activeFileFolder.pop()
+  const _oldactiveFileFolder = activeFileFolder.join('/')
+  console.log('_oldactiveFileFolder')
 }
