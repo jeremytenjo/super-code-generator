2c4fdf4c4dad0d86545bef772b896b22de0aecdf
Jeremy Tenjo <tenjojeremy@gmail.com>
2025-02-04 10:26:23 -0500

    Update VSCode and TypeScript configurations to exclude 'out' directory and set 'outDir' to 'dist'

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,35 +9,8 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   {
-    ignores: ['out', 'dist', 'scripts'],
+    ignores: ['dist', 'scripts'],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
 )
-
-// {
-//   "env": {
-//     "browser": false,
-//     "commonjs": true,
-//     "es6": true,
-//     "node": true,
-//     "mocha": true
-//   },
-//   "parserOptions": {
-//     "ecmaVersion": "latest",
-//     "ecmaFeatures": {
-//       "jsx": true
-//     },
-//     "sourceType": "module"
-//   },
-//   "ignorePatterns": ["lib", "app", "out"],
-//   "rules": {
-//     "no-const-assign": "warn",
-//     "no-this-before-super": "warn",
-//     "no-undef": "error",
-//     "no-unreachable": "warn",
-//     "no-unused-vars": "error",
-//     "constructor-super": "warn",
-//     "valid-typeof": "warn"
-//   }
-// }
