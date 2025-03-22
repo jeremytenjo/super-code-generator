da9703574eab7b9f592409fc46dc3ec77d3ac714
Jeremy Tenjo <tenjojeremy@gmail.com>
2025-03-22 10:03:40 -0400

    Remove unused CSS and TSX files for Asdf and Hi components

--- example/app/hi/asdf/asdf/asdf.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import styles from './asdf.css'
-
-export default function Asdf() {
-  return <div className={`styles.wrapper`}>asdf</div>
-}
--- example/app/hi/hi.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import styles from './hi.css'
-
-export default function Hi() {
-  return <div className={`styles.wrapper`}>hi</div>
-}
